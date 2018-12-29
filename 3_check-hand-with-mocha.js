494307c77b30fdc2065e7bc2f7aa8eaa96424786
CaiLI <dy5025115@gmail.com>
2018-12-29 19:46:15 +0800

    +(3_check-hand-with-mocha.js) 增加 isPair()的函式內容 ，得到 multiplesIn is not defined的錯誤

--- 3_check-hand-with-mocha.js
+++ 3_check-hand-with-mocha.js
@@ -7,7 +7,16 @@ var checkHand = function (hand) {
         return 'three of a kind';
     }
 };
-function isPair () { };
+function isPair () {
+    return multiplesIn(hand) === 2;
+};
+
+describe('isPair()', function () {
+    it('找到一組對子', function () {
+        var result = isPair(['2-H', '3-C', '4-D', '5-H', '2-C']);
+        wish(result)
+    })
+});
 describe('checkHand()', function () {
     it('處理對子', function () {
         var result = checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']);
@@ -19,10 +28,4 @@ describe('checkHand()', function () {
         var result = checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']);
         wish(result === 'three of a kind');
     });
-});
-describe('isPair()', function () {
-    it('找到一組對子', function () {
-        var result = isPair(['2-H', '3-C', '4-D', '5-H', '2-C']);
-        wish(result)
-    })
 });
