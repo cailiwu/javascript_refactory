6627ddda9ab8bd7e07e958e0bbd2b20972b03f34
CaiLI <dy5025115@gmail.com>
2018-12-29 19:12:29 +0800

    +(2_check_hand.js) 寫足夠通過測試的簡易程式碼，來修復它

--- 2_check_hand.js
+++ 2_check_hand.js
@@ -1,7 +1,13 @@
 // console.log(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']));
 // console.log(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']));
-var checkHand = function () {
-    return 'pair';
+var checkHand = function (hand) {
+    if (hand[0] === '2-H' && hand[1] === '3-C'
+        && hand[2] === '4-D' && hand[3] === '5-H'
+        && hand[4] === '2-C') {
+        return 'pair';
+    } else {
+        return 'three of a kind';
+    }
 };
 var wish = require('wish');
 wish(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']) === 'pair');
