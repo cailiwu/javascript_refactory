553e8740ce37d50710a6691271c63fe062ddb5cb
CaiLI <dy5025115@gmail.com>
2018-12-29 18:47:22 +0800

    +(2_check_hand.js) 使用node 斷言庫來斷言函式

--- 2_check_hand.js
+++ 2_check_hand.js
@@ -1,2 +1,6 @@
-console.log(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']));
-console.log(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']));
+// console.log(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']));
+// console.log(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']));
+var assert = require('assert');
+assert(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']) === 'pair');
+assert(checkHand(['3-H', '3-C', '3-D',
+    '5-H', '2-H']) === 'three of a kind');
