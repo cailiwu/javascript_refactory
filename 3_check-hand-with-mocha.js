52bd0094ae0a972696760410f551824ee5715844
CaiLI <dy5025115@gmail.com>
2018-12-29 19:30:30 +0800

    +(3_check-hand-with-mocha.js) 增加另一個對子的測試用例，會得到一個失敗的測試與一個成功的測試

--- 3_check-hand-with-mocha.js
+++ 3_check-hand-with-mocha.js
@@ -13,6 +13,8 @@ describe('checkHand()', function () {
     it('處理對子', function () {
         var result = checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']);
         wish(result === 'pair');
+        var anotherResult = checkHand(['3-H', '3-C', '4-D', '5-H', '2-C']);
+        wish(anotherResult === 'pair');
     });
     it('處理三條', function () {
         var result = checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']);
