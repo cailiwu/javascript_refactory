207c15a15f8824a4648d568034e95e218a2c9e25
CaiLI <dy5025115@gmail.com>
2018-12-29 20:34:15 +0800

    modify coding style

--- 3_check-hand-with-mocha.js
+++ 3_check-hand-with-mocha.js
@@ -8,11 +8,14 @@ var checkHand = function (hand) {
         return 'three of a kind';
     }
 };
+
 function isPair (hand) {
     return multiplesIn(hand) === 2;
 };
+
 function highestCount (values) {
     var counts = {};
+
     values.forEach(function (value, index) {
         counts[value] = 0;
         if (value == values[0]) {
@@ -31,19 +34,26 @@ function highestCount (values) {
             counts[value] = counts[value] + 1;
         };
     });
+
     var totalCounts = Object.keys(counts).map(function (key) {
         return counts[key];
     });
-    return totalCounts.sort(function (a, b) { return b - a })[0];
+
+    return totalCounts.sort(function (a, b) {
+        return b - a;
+    })[0];
 };
+
 function valuesFromHand (hand) {
     return hand.map(function (card) {
         return card.split('-')[0];
     });
 };
+
 function multiplesIn (hand) {
     return highestCount(valuesFromHand(hand));
 };
+
 describe('valueFromhHand()', function () {
     it('只從手排中返回點數', function () {
         var result = valuesFromHand(['2-H', '3-C', '4-D', '5-H', '2-C']);
