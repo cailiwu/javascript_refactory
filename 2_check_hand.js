8c2f3647c67342615b25cc1d9cf9f4b5a2f64aab
CaiLI <dy5025115@gmail.com>
2018-12-29 18:51:48 +0800

    +(2_check_hand.js) 增加一個checkHand的函式

--- 2_check_hand.js
+++ 2_check_hand.js
@@ -1,5 +1,6 @@
 // console.log(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']));
 // console.log(checkHand(['3-H', '3-C', '3-D', '5-H', '2-H']));
+var checkHand = function () { };
 var assert = require('assert');
 assert(checkHand(['2-H', '3-C', '4-D', '5-H', '2-C']) === 'pair');
 // assert(checkHand(['3-H', '3-C', '3-D',
